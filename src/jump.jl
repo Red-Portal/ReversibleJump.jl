c31a7f72c066bcc940000a425cb705231495f70b
Ray Kim <msca8h@naver.com>
2023-11-10 01:53:08 +0000

    add jump

---
+++ src/jump.jl
@@ -0,0 +1,27 @@
+
+function step_jump(
+    rng     ::Random.AbstractRNG,
+    move    ::AbstractJumpMove,
+    proposal::AbstractJumpProposal,
+    prev    ::RJState,
+    mcmc,
+    model,
+    move_kernel,
+)
+    prop, ℓr = propose_jump(rng, move, proposal, prev, mcmc, model)
+    k′, k     = prop.order, prev.order 
+    q_k′, q_k = move_kernel(k, k′), move_kernel(k′, k)
+
+    ℓα = min(0, ℓr - log(q_k′/q_k))
+    α  = exp(ℓα)
+        
+    stats = (move = move, jump_rate=α,)
+    
+    if  log(rand(rng)) < ℓα
+        stats′ = merge(stats, (has_jumped=true,))
+        @set prop.stats = stats′
+    else
+        stats′ = merge(stats, (has_jumped=false,))
+        @set prev.stats = stats′
+    end
+end
